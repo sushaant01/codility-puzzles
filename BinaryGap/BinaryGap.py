f10e2288c4b935037e7c653b5dca3e455d16f1fc
sushaant01 <sushant_01@outlook.com>
2022-08-03 17:04:43 +0100

    BinaryGap Puzzle

---
+++ BinaryGap/BinaryGap.py
@@ -0,0 +1,18 @@
+def BinaryGap(N):
+    # write your code in Python 3.6
+    binVal = bin(N)    
+    stringVal = binVal[2:]
+    returnVal = 0
+    counterVal = 0
+    for var in stringVal :
+        if var == "0" :
+            counterVal = counterVal + 1
+        else :
+            if returnVal < counterVal :
+                returnVal = counterVal 
+            counterVal = 0
+    return returnVal
+pass
+
+print(BinaryGap(15))
+print(BinaryGap(152))
