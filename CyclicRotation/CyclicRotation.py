89f9cc7a26072258578f2a5e65f0901d6e765efe
sushaant01 <sushant_01@outlook.com>
2022-08-03 17:19:23 +0100

    CyclicRotation

---
+++ CyclicRotation/CyclicRotation.py
@@ -0,0 +1,21 @@
+def CyclicRotation(A, K):
+    # write your code in Python 3.6
+    if  len(A) > 0 :
+        counter = (K % len(A) ) 
+        B = [0]*len(A)
+        if counter >= 0 :
+            for var in A :
+                if counter >= len(A) :
+                    counter = counter - len(A) 
+                B[counter] = var
+                counter = counter + 1
+        return B
+    else:
+        return A
+pass
+
+print (CyclicRotation([3, 8, 9, 7, 6], 1))
+print (CyclicRotation([3, 8, 9, 7, 6], 2))
+print (CyclicRotation([3, 8, 9, 7, 6], 3))
+print (CyclicRotation([3, 8, 9, 7, 6], 4))
+print (CyclicRotation([3, 8, 9, 7, 6], 5))
